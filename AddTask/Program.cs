47cde21f84cff6cdcdc3d0255c9c78070903b604
Valera <vlaus0@gmail.com>
2023-03-18 21:51:02 +0200

    Добавьте файлы проекта.

---
+++ AddTask/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTask
+{
+    /*
+    Використовуючи Visual Studio, створіть проект по шаблону Console Application.
+    Створіть клас Calculator.
+    В тілі класу створіть чотири методи для арифметичних дій: Add – додаванн, Sub – віднімання, Mul – множення, Div – ділення.
+    Метод ділення має робити перевірку ділення на нуль, якщо перевірка не проходить, згенерувати виключення.
+    Користувач вводить значення, над якими хоче провести операцію и вибрати саму операцію. 
+    При виникненні помилок повинні викидатися виключення.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число A");
+            int operand1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите число B");
+            int operand2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите операцию");
+            string choice = Console.ReadLine();
+
+            Calculator calculator = new Calculator();
+
+            int? result = 0;//Если тип переменной указан со знаком "?" то переменная может быть пустой 
+            bool calculation = true;
+            switch (choice)
+            {
+                case "+":
+                    {
+                        result = calculator.Add(operand1, operand2); //Вызов метода сложения чисел
+                        break;
+                    }
+                case "-":
+                    {
+                        result = calculator.Sub(operand1, operand2); //Вызов метода вычитания чисел
+                        break;
+                    }
+                case "*":
+                    {
+                        result = calculator.Mul(operand1, operand2); //Вызов метода умножения числе
+                        break;
+                    }
+                case "/":
+                    {
+                        result = calculator.Div(operand1, operand2);//Вызов метода деления чисел
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Вы ввели недопустимое значение.");
+                        Console.ReadKey();
+                        calculation = false;
+                        break;
+                    }
+            }
+
+            if (calculation && result != null)
+            {
+                Console.WriteLine("{0} {1} {2} = {3}", operand1, choice, operand2, result);
+            }
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    /*
+    Використовуючи Visual Studio, створіть проект за шаблоном Console Application. 
+    Потрібно описати структуру з іменем Price, що містить такі поля:
+        • назва товару;
+        • назва магазину, де продається товар;
+        • вартість товару у гривнях.
+    Написати програму, яка виконує такі дії:
+        • введення з клавіатури даних до масиву, що складається з двох елементів типу Price 
+         (записи мають бути впорядковані в алфавітному порядку за назвами магазинів);
+        • виведення на екран інформації про товари, що продаються в магазині, назва якого введена з клавіатури 
+         (якщо такого магазину немає, вивести виняток).
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PriceTable priceTable = new PriceTable(); //Создание экземпляра класса PriceTable и инициализация конструктором по умолчанию
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(priceTable[i]); //Отображение содержимого priceTable
+            }
+
+            Console.WriteLine("Введите номер товара для поиска:");
+            string product = Console.ReadLine();
+
+            Console.WriteLine(priceTable[product]); //Отображение результата поиска
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
